8d0700cab7ce0b9b3be873842fda79015edb5952
CaioSanches7 <78539712+CaioSanches7@users.noreply.github.com>
2021-02-25 20:19:02 -0300

    criação modelo main.c
    Criação do modelo main.c
    modelo com o conteúdo básico e essencial

---
+++ Modelo Inicial Projetos/main.c
@@ -0,0 +1,12 @@
+//Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+// \ barra invertida pois não tenho no teclado
+int main (void){
+    system("cls");//limpando terminal
+
+
+
+    printf("\n\n\n\n\n\n");
+}
